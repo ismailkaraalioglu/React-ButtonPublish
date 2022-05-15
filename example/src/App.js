b6f02f8f3abc3fd1dc13137d53cc48dccb0c4f90
Ismail Karaalioglu <ismailkaraalioglu@gmail.com>
2022-05-15 20:14:11 +0300

    First Commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
-import { ExampleComponent } from 'designdev'
+import { Button, ExampleComponent } from 'designdev'
 import 'designdev/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <>
+      <ExampleComponent text='Create React Library Example 😄' />
+      <Button text='Click' />
+    </>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -4,3 +4,7 @@ import styles from './styles.module.css'
 export const ExampleComponent = ({ text }) => {
   return <div className={styles.test}>Example Component: {text}</div>
 }
+
+export const Button = (props) => {
+  return <button className={type = "primary" && styles.primary} {...props}>{props.text}</button>
+}
